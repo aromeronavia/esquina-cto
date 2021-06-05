d3fdfdf5faf117c0a60fab55763437288bae7ebe
Alberto Romero <aromeronavia@gmail.com>
2021-06-05 15:41:24 -0500

    Add keywords to meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,12 @@ export default {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" }
+      { hid: "description", name: "description", content: "" },
+      {
+        name: "keywords",
+        content:
+          "esquinacto, esquina cto, tech ctos,cto tech, tech, mexico, latam tech"
+      }
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
